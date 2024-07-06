c06a8b0e924282789a569562fb5b014e06e68f17
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-06 16:00:14 +0330

    fixed style Technology for display flex

--- src/components/template/home/Technology/Technology.jsx
+++ src/components/template/home/Technology/Technology.jsx
@@ -5,7 +5,7 @@ function Technology() {
   return (
     <section className="block w-full bg-success mt-12 py-8">
             <div className="container">
-                <div className="sm:flex sm:flex-col-reverse   md:grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
+                <div className="flex flex-col-reverse   md:grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
                     <div className="block mt-20">
                         <h3 className="block font-dana-bold text-md text-secondary">موارد جالب درباره فناروی</h3>
                         <p className="block my-6 font-dana-medium text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
