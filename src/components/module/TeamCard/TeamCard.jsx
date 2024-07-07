b0650c9f6dbf0c58cecf52ea8ab083b384b7f393
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-07 16:40:59 +0330

    create section TeamCard for team

---
+++ src/components/module/TeamCard/TeamCard.jsx
@@ -0,0 +1,15 @@
+
+
+function TeamCard() {
+  return (
+    <div className="block bg-white rounded-sm relative shadow-lg shadow-gray-400">
+    <img src="/assets/images/team-1.jpg" alt="team - image" className="w-full" />
+    <div className="block text-center my-4">
+        <h5 className="block font-dana-bold text-md text-secondary">الکس ماکسول</h5>
+        <span className="block mt-2 text-primary">مدیر عامل</span>
+    </div>
+</div>
+  )
+}
+
+export default TeamCard
