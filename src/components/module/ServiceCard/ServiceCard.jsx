96df31eb79bd97f9f9a9bd22e64c6876393284d0
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-07 14:34:10 +0330

    fixed duration all project

--- src/components/module/ServiceCard/ServiceCard.jsx
+++ src/components/module/ServiceCard/ServiceCard.jsx
@@ -5,7 +5,7 @@ function ServiceCard() {
   return (
     <Link to='' className="block relative mb-8">
     <img src="/assets/images/s1.png" alt="services image" className="block w-full rounded-lg" />
-    <div className="block absolute text-center bg-white w-[80%] h-24 mr-8 p-2 -bottom-8 rounded text-secondary z-50 delay-300  hover:text-white hover:bg-primary">
+    <div className="block absolute text-center bg-white w-[80%] h-24 mr-8 p-2 -bottom-8 rounded text-secondary z-50 ease-linear duration-300  hover:text-white hover:bg-primary">
         <span className="block font-dana-bold text-2xl">توسعه دهنده وب</span>
         <span className="block text-base font-semibold">طراحی وب سایت</span>
     </div>
