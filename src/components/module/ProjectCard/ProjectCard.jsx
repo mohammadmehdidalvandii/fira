9d05cf20c13d1363069cceaabca9d8eb7bb396e4
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-13 15:29:29 +0330

    fixed projectCard styles

--- src/components/module/ProjectCard/ProjectCard.jsx
+++ src/components/module/ProjectCard/ProjectCard.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 function ProjectCard() {
   return (
-    <Link to='/'  className="block bg-white rounded-lg shadow-lg shadow-gray-300 ">
-    <img src="/assets/images/project.png" alt="project image" className="block" />
+    <Link to='/'  className="block w-full bg-white rounded-lg shadow-lg shadow-gray-300 ">
+    <img src="/assets/images/project.png" alt="project image" className="block w-full" />
     <div className="block py-4 px-2 text-secondary ease-out duration-300 hover:bg-primary hover:text-white cursor-pointer">
         <h6 className="block font-dana-bold text-2xl  ">توسعه دهنده وب</h6>
         <span className="block  mt-4 ">طراحی وب سایت </span>
