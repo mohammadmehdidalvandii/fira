54d230b423b7349e35b13614cbf95a847cc3418b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-13 14:56:32 +0330

    create page project

---
+++ src/page/Project/Project.jsx
@@ -0,0 +1,14 @@
+import Breadcrumb from "../../components/module/Breadcrumb/Breadcrumb"
+import Projects from "../../components/template/project/Projects/Projects"
+
+
+function Project() {
+  return (
+    <>
+           <Breadcrumb title="پروژه ها" subtitle="پروژه ها" />
+           <Projects/>
+    </>
+  )
+}
+
+export default Project
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import Home from "./page/Home/Home";
 import About from "./page/About/About";
 import Services from "./page/Services/Services";
+import Project from "./page/Project/Project";
 
 const routes = [
     {path:"/" , element:<Home/>},
     {path:"/About" , element:<About/>},
-    {path:"/Services" , element:<Services/>}
+    {path:"/Services" , element:<Services/>},
+    {path:"/Project" , element:<Project/>},
 ]
 
 export default routes;
