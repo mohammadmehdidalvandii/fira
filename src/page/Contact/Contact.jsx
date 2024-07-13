ec0da998dcb74a2af1bcca49dffdae1d99a99215
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-13 16:16:18 +0330

    create page-contact

---
+++ src/page/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import Breadcrumb from '../../components/module/Breadcrumb/Breadcrumb'
+
+function Contact() {
+  return (
+    <>
+    <Breadcrumb title="تماس با ما" subtitle="تماس با ما"/>
+    </>
+  )
+}
+
+export default Contact
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import About from "./page/About/About";
 import Services from "./page/Services/Services";
 import Project from "./page/Project/Project";
 import Articles from "./page/Articles/Articles";
+import Contact from "./page/Contact/Contact";
 
 const routes = [
     {path:"/" , element:<Home/>},
@@ -10,6 +11,7 @@ const routes = [
     {path:"/Services" , element:<Services/>},
     {path:"/Project" , element:<Project/>},
     {path:"/Articles" , element:<Articles/>},
+    {path:"/Contact" , element:<Contact/>},
 ]
 
 export default routes;
