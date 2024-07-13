6743bf028fd2f1238c803ab8d59b03eaa3f5d86b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-13 15:26:12 +0330

    create section projectCard

---
+++ src/components/module/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+function ProjectCard() {
+  return (
+    <Link to='/'  className="block bg-white rounded-lg shadow-lg shadow-gray-300 ">
+    <img src="/assets/images/project.png" alt="project image" className="block" />
+    <div className="block py-4 px-2 text-secondary ease-out duration-300 hover:bg-primary hover:text-white cursor-pointer">
+        <h6 className="block font-dana-bold text-2xl  ">توسعه دهنده وب</h6>
+        <span className="block  mt-4 ">طراحی وب سایت </span>
+    </div>
+</Link>
+  )
+}
+
+export default ProjectCard
