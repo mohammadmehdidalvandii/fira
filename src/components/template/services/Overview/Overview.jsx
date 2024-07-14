f58702a9a3f201346037c9312963df4c73eacb6f
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-14 16:04:15 +0330

    Creare section overview for service-details page

---
+++ src/components/template/services/Overview/Overview.jsx
@@ -0,0 +1,48 @@
+
+
+function Overview() {
+  return (
+    <section className="block mt-20 w-full">
+        <div className="container">
+            <div className="block">
+                <div className="flex flex-col-reverse md:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+                    <div className="block">
+                        <h4 className="block font-dana-bold text-md text-secondary">محصولات استثنایی</h4>
+                        <p className="block mt-4 text-textColor line leading-normal">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        <div className="block mt-6">
+                            <h6 className="block font-dana-bold text-2xl text-secondary">دلایل ایجاد کسب و کار</h6>
+                            <p className="block mt-3 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        </div>
+                        <div className="block mt-6">
+                            <h6 className="block font-dana-bold text-2xl text-secondary">بازاریابی دیجیتال</h6>
+                            <p className="block mt-3 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        </div>
+                    </div>
+                    <div className="block">
+                        <img src="/assets/images/sd-1.jpg" alt="" className="block w-full rounded-md" />
+                    </div>
+                </div>
+                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mt-12">
+                    <div className="block">
+                        <img src="/assets/images/sd-2.jpg" alt="" className="block w-full rounded-md" />
+                    </div>
+                    <div className="block">
+                    <h4 className="block font-dana-bold text-md text-secondary"> ارائه خدمات با کیفیت</h4>
+                        <p className="block mt-4 text-textColor line leading-normal">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        <div className="block mt-6">
+                            <h6 className="block font-dana-bold text-2xl text-secondary">دلایل ایجاد کسب و کار</h6>
+                            <p className="block mt-3 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        </div>
+                        <div className="block mt-6">
+                            <h6 className="block font-dana-bold text-2xl text-secondary">بازاریابی دیجیتال</h6>
+                            <p className="block mt-3 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Overview
--- src/page/ServicesDetails/ServicesDetails.jsx
+++ src/page/ServicesDetails/ServicesDetails.jsx
@@ -1,11 +1,13 @@
 import Breadcrumb from '../../components/module/Breadcrumb/Breadcrumb';
 import Content from '../../components/template/services/Content/Content';
+import Overview from '../../components/template/services/Overview/Overview';
 
 function ServicesDetails() {
   return (
     <>
         <Breadcrumb title="جزئیات خدمات" subtitle="جزئیات خدمات"/>
         <Content/>
+        <Overview/>
     </>
   )
 }
