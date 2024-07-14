abc33b02d42cb7d1155dce4b040c5c1118f98c45
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-14 15:37:28 +0330

    create section Content for service-details page

---
+++ src/components/template/services/Content/Content.jsx
@@ -0,0 +1,19 @@
+
+
+function Content() {
+  return (
+    <section className="block w-full mt-12">
+        <div className="container">
+            <div className="block w-full">
+                <img src="/assets/images/bg-sd.jpg" alt="bg-images" className="block w-full rounded-lg" />
+                <div className="block mt-6">
+                    <h3 className="block font-dana-bold text-md text-secondary">خدمات با کیفتی  را ارائه می دهیم</h3>
+                    <p className="block text-textColor mt-4">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Content
--- src/page/ServicesDetails/ServicesDetails.jsx
+++ src/page/ServicesDetails/ServicesDetails.jsx
@@ -1,9 +1,11 @@
 import Breadcrumb from '../../components/module/Breadcrumb/Breadcrumb';
+import Content from '../../components/template/services/Content/Content';
 
 function ServicesDetails() {
   return (
     <>
         <Breadcrumb title="جزئیات خدمات" subtitle="جزئیات خدمات"/>
+        <Content/>
     </>
   )
 }
