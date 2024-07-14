291a5e6fb957ef984e2167bf6d9e9c5dcaf3d9b6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-14 15:31:23 +0330

    Create Services-details page

---
+++ src/page/ServicesDetails/ServicesDetails.jsx
@@ -0,0 +1,11 @@
+import Breadcrumb from '../../components/module/Breadcrumb/Breadcrumb';
+
+function ServicesDetails() {
+  return (
+    <>
+        <Breadcrumb title="جزئیات خدمات" subtitle="جزئیات خدمات"/>
+    </>
+  )
+}
+
+export default ServicesDetails
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import Home from "./page/Home/Home";
 import About from "./page/About/About";
 import Services from "./page/Services/Services";
+import ServicesDetails from "./page/ServicesDetails/ServicesDetails";
 import Project from "./page/Project/Project";
 import Articles from "./page/Articles/Articles";
 import Contact from "./page/Contact/Contact";
@@ -11,6 +12,7 @@ const routes = [
     {path:"/" , element:<Home/>},
     {path:"/About" , element:<About/>},
     {path:"/Services" , element:<Services/>},
+    {path:"/Services-D" , element:<ServicesDetails/>},
     {path:"/Project" , element:<Project/>},
     {path:"/Articles" , element:<Articles/>},
     {path:"/Contact" , element:<Contact/>},
