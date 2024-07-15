d2fc6a9fef28b7cb86ee532ddedffa59996b998c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-15 14:47:37 +0330

    create section Content fot project-details page

---
+++ src/components/template/project/Content/Content.jsx
@@ -0,0 +1,34 @@
+import { FaCheck } from "react-icons/fa"
+
+
+function Content() {
+  return (
+    <section className="block w-full mt-12">
+        <div className="container">
+            <div className="block w-full">
+                <div className="block">
+                    <h4 className="block font-dana-bold text-4xl text-secondary">تجزیه و تحلیل</h4>
+                    <p className="block text-textColor mt-4 ">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+                <div className="block mt-8">
+                    <div className="flex items-center">
+                        <span className="block text-primary text-2xl"><FaCheck/></span>
+                        <h6 className="block font-dana-bold text-xl mr-3">توسعه شبکه</h6>
+                    </div>
+                    <p className="block text-textColor mt-3">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+                <div className="block mt-8">
+                    <div className="flex items-center">
+                        <span className="block text-primary text-2xl"><FaCheck/></span>
+                        <h6 className="block font-dana-bold text-xl mr-3">تعریف توسعه</h6>
+                    </div>
+                    <p className="block text-textColor mt-3">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+                <p className="block  text-textColor mt-6 text-2xl leading-8">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Content
--- src/page/ProjectDetails/ProjectDetails.jsx
+++ src/page/ProjectDetails/ProjectDetails.jsx
@@ -1,4 +1,5 @@
 import Breadcrumb from "../../components/module/Breadcrumb/Breadcrumb"
+import Content from "../../components/template/project/Content/Content"
 import Gallery from "../../components/template/project/Gallery/Gallery"
 
 
@@ -7,6 +8,7 @@ function ProjectDetails() {
     <>
     <Breadcrumb title='جزئیات پروژه' subtitle='جزئیات پروژه'/>
     <Gallery/>
+    <Content/>
     </>
   )
 }
