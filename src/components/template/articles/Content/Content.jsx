4edd7a1a5a790ccf5710a96e0109d4f367838bbb
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-15 15:16:20 +0330

    create section Content for articleDetails page

---
+++ src/components/template/articles/Content/Content.jsx
@@ -0,0 +1,52 @@
+function Content() {
+  return (
+    <section className="block w-full mt-12">
+      <div className="container">
+        <div className="block">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+            <div className="block">
+              <img
+                src="/assets/images/ad-1.jpg"
+                alt="article-image"
+                className="block rounded-lg"
+              />
+            </div>
+            <div className="block">
+              <img
+                src="/assets/images/ad-1.jpg"
+                alt="article-image"
+                className="block rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="block mt-8">
+                <ul className="flex items-center gap-4">
+                    <li className="flex items-center">
+                        <span className="block font-dana-bold text-xl text-secondary">نوشته شد در :</span>
+                        <span className="block font-dana-bold text-sm text-textColor mr-3">28 اسفند 1402</span>
+                    </li>
+                    <li className="flex items-center">
+                        <span className="block font-dana-bold text-xl text-secondary">ارسال شد توسط :</span>
+                        <span className="block font-dana-bold text-sm text-textColor mr-3">جان اندرسون</span>
+                    </li>
+                </ul>
+                <div className="block mt-8">
+                    <h4 className="block font-dana-bold text-4xl text-secondary">ایده ها و تجربیات کسب و کار</h4>
+                    <p className="block mt-4 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                    <p className="block mt-4 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                    <p className="block mt-4 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+                <div className="block mt-8">
+                    <h4 className="block font-dana-bold text-4xl text-secondary">روش کسب و کار الکترونیک</h4>
+                    <p className="block mt-4 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                    <p className="block mt-4 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                    <p className="block mt-4 text-textColor">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Content;
--- src/page/ArticleDetails/ArticleDetails.jsx
+++ src/page/ArticleDetails/ArticleDetails.jsx
@@ -1,10 +1,12 @@
 import Breadcrumb from "../../components/module/Breadcrumb/Breadcrumb"
+import Content from "../../components/template/articles/Content/Content"
 
 
 function ArticleDetails() {
   return (
     <>
     <Breadcrumb title='جزئیات مقاله' subtitle='جزئیات مقاله'/>
+    <Content/>
     </>
   )
 }
