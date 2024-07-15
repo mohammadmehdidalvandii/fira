bffd597463b36b68da1ad8608fb6926e9d0dbf22
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-15 14:39:03 +0330

    create section gallery for project-deatils-page

--- src/page/ProjectDetails/ProjectDetails.jsx
+++ src/page/ProjectDetails/ProjectDetails.jsx
@@ -1,10 +1,12 @@
 import Breadcrumb from "../../components/module/Breadcrumb/Breadcrumb"
+import Gallery from "../../components/template/project/Gallery/Gallery"
 
 
 function ProjectDetails() {
   return (
     <>
     <Breadcrumb title='جزئیات پروژه' subtitle='جزئیات پروژه'/>
+    <Gallery/>
     </>
   )
 }
